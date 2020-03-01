b4c281ad2934941221b830cf7812564c30daa8d2
AntonKopysov <antohapp@gmail.com>
2020-03-01 15:44:11 +0400

    add start menu

--- src/main/java/me/tlgm/Comands.java
+++ src/main/java/me/tlgm/Comands.java
@@ -5,6 +5,7 @@ public class Comands {
     private static final String COMMAND_INIT_CHAR = "/";
 
     public static final String HELP = COMMAND_INIT_CHAR + "help";
+    public static final String START = COMMAND_INIT_CHAR + "start";
 
     public static final String ENTER_FIO = COMMAND_INIT_CHAR + "fio";
     public static final String ENTER_PASSPORT = COMMAND_INIT_CHAR + "passport";
