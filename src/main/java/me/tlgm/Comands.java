57ea49536a99e978ad7658aa6f4938f5778d77b6
AntonKopysov <antohapp@gmail.com>
2020-03-01 23:09:55 +0400

    fix to main

--- src/main/java/me/tlgm/Comands.java
+++ src/main/java/me/tlgm/Comands.java
@@ -6,7 +6,8 @@ public class Comands {
 
     public static final String HELP = COMMAND_INIT_CHAR + "help";
     public static final String START = COMMAND_INIT_CHAR + "start";
-    public static final String TO_MAIN = COMMAND_INIT_CHAR + "В главное меню";
+
+    public static final String TO_MAIN = "В главное меню";
 
     public static final String ENTER_FIO = COMMAND_INIT_CHAR + "fio";
     public static final String ENTER_PASSPORT = COMMAND_INIT_CHAR + "passport";
